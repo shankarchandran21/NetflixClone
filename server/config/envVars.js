f0b917513d3ba2b3a609c0870629d9f59efa048d
Shankar <72694164+shankarchandran21@users.noreply.github.com>
2024-10-17 10:12:59 +0530

    login logout signup completed

--- server/config/envVars.js
+++ server/config/envVars.js
@@ -5,4 +5,7 @@ dotenv.config()
 export const ENV_VARS ={
     MONGO_URL : process.env.MONGO_URL,
     PORT:process.env.PORT||5000,
+    JWT_SECRET:process.env.JWT_SECRET,
+    NODE_ENV:process.env.NODE_ENV,
+    TMDB_API_KEY : process.env.TMDB_API_KEY,
 }
--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ import { connectDB } from './config/db.js';
 const app = express();
 const PORT = ENV_VARS.PORT
 
-app.use("/api/v1/auth",authRouter)
 app.use(express.json()) // it's allows you parse body json
+app.use("/api/v1/auth",authRouter)
 app.get("", (req, res) => {
     res.send("Server is running 123")
 })
